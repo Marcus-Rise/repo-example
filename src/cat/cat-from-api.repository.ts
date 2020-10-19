85218dc4c06acd361cd46d8301864f62a956adba
Ilya Konstantinov <ilykonst95@gmail.com>
2020-10-19 21:09:45 +0400

    fix(cat-from-api.repository.ts): catch errors

--- src/cat/cat-from-api.repository.ts
+++ src/cat/cat-from-api.repository.ts
@@ -3,14 +3,28 @@ import {ICatRepository} from "./cat.repository-interface";
 
 export class CatFromApiRepository implements ICatRepository {
     async get(filter: { name?: string; size?: number }): Promise<ICat | null> {
-        const response = await fetch(`/api/cat?name=${filter.name}&size=${filter.size}`)
+        let res: ICat | null = null;
 
-        return response.json();
+        await fetch(`/api/cat?name=${filter.name}&size=${filter.size}`)
+            .then((res) => res.json())
+            .then((data) => {
+                res = data;
+            })
+            .catch(console.error);
+
+        return res;
     }
 
     async list(): Promise<ICat[]> {
-        const response = await fetch(`/api/cat`)
+        let res: ICat[] = [];
 
-        return response.json();
+        await fetch(`/api/cat`)
+            .then((res) => res.json())
+            .then((data) => {
+                res = data;
+            })
+            .catch(console.error);
+
+        return res;
     }
 }
